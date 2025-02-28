68db887ad0e2e0716dafcdaa46e1cbcb8a8e5490
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-02-28 22:27:11 +0100

    [VarExporter] Leverage native lazy objects

--- Tests/Validator/Constraints/UniqueEntityValidatorTest.php
+++ Tests/Validator/Constraints/UniqueEntityValidatorTest.php
@@ -223,6 +223,9 @@ class UniqueEntityValidatorTest extends ConstraintValidatorTestCase
         yield 'Named arguments' => [new UniqueEntity(message: 'myMessage', fields: ['name'], em: 'foo')];
     }
 
+    /**
+     * @group legacy
+     */
     public function testValidateEntityWithPrivatePropertyAndProxyObject()
     {
         $entity = new SingleIntIdWithPrivateNameEntity(1, 'Foo');

0b89ae1394ce041a863191cfafb6e078fc12d9db
Michael Käfer <michael.kaefer1@gmx.at>
2025-10-21 13:38:08 +0200

    [DoctrineBridge] Remove unnecassary check

--- Form/DoctrineOrmTypeGuesser.php
+++ Form/DoctrineOrmTypeGuesser.php
@@ -125,7 +125,7 @@ class DoctrineOrmTypeGuesser implements FormTypeGuesserInterface
     public function guessMaxLength(string $class, string $property): ?ValueGuess
     {
         $ret = $this->getMetadata($class);
-        if ($ret && isset($ret[0]->fieldMappings[$property]) && !$ret[0]->hasAssociation($property)) {
+        if ($ret && isset($ret[0]->fieldMappings[$property])) {
             $mapping = $ret[0]->getFieldMapping($property);
 
             $length = $mapping instanceof FieldMapping ? $mapping->length : ($mapping['length'] ?? null);
@@ -145,7 +145,7 @@ class DoctrineOrmTypeGuesser implements FormTypeGuesserInterface
     public function guessPattern(string $class, string $property): ?ValueGuess
     {
         $ret = $this->getMetadata($class);
-        if ($ret && isset($ret[0]->fieldMappings[$property]) && !$ret[0]->hasAssociation($property)) {
+        if ($ret && isset($ret[0]->fieldMappings[$property])) {
             if (\in_array($ret[0]->getTypeOfField($property), [Types::DECIMAL, Types::FLOAT], true)) {
                 return new ValueGuess(null, Guess::MEDIUM_CONFIDENCE);
             }

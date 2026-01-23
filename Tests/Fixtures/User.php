5664f0b549d2a509f9ac0116ebe8ce248aeec97b
Christian Flothmann <christian.flothmann@open.de>
2026-01-23 18:50:14 +0100

    revert clean ups that make tests incompatible with Symfony 7.4

--- Tests/Fixtures/User.php
+++ Tests/Fixtures/User.php
@@ -45,6 +45,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this->name;
     }
 
+    #[\Deprecated]
+    public function eraseCredentials(): void
+    {
+    }
+
     public function equals(UserInterface $user)
     {
     }
